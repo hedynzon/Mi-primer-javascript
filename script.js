1c45d02c1d454253af7c2c5b812ff2d774f4157d
hedynzon <edinson.raymundo@gmail.com>
2026-06-23 22:35:47 -0500

    actualizando...

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hola, desde JavaScript!");
+let nombre="Edinson Raymundo"
+
+console.log(nombre)
+console.log(typeof nombre)
+
+const curso="Code 101"
+
+console.log(curso)
+
+nombre = "Edison Raymundo"
+
+console.log(nombre)
+
+console.log(2 + 2);
+
+console.log('2' + '2');
+
+let nombre = "Ada"
+let edad = 28
+let programadora = true
+
+console.log(nombre, typeof nombre)
+console.log(edad, typeof edad)
+console.log(programadora, typeof programadora)
+let numero = prompt("Ingrese un número")
+console.log(numero)
+console.log(typeof numero)
+console.log(Number(numero) + 10)
+
+let msg1 = "Hola, soy " + nombre + " y tengo " + edad + " años y soy programadora"
+console.log(msg1);
+
+let msg2 = `Hola, soy ${nombre} y tengo ${edad} años y soy programadora`
+console.log(msg2);
